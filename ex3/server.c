39fb1af3348cf0124f59ce2b1a585e75d860f455
Pradeep KM <pradeepkm6122004@gmail.com>
2024-10-17 19:22:39 +0530

    feat: file transfer in client server

--- ex3/server.c
+++ ex3/server.c
@@ -7,47 +7,70 @@
 int main() {
     int fd, newfd;
     struct sockaddr_in saddr, caddr;
-    int addrlen = sizeof(saddr);
+    int addrlen = sizeof(caddr);
     char buff[1024], fname[1024];
-    
+
     fd = socket(AF_INET, SOCK_STREAM, 0);
-    int opt = 1;
-    
-    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-    if(fd < 0) {
+    if (fd < 0) {
         printf("SOCKET FAILURE\n");
-        return 0;
+        return 1;
     }
+
+    int opt = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
     saddr.sin_family = AF_INET;
     saddr.sin_port = htons(7228);
     saddr.sin_addr.s_addr = INADDR_ANY;
 
-    if(bind(fd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0) {
+    if (bind(fd, (struct sockaddr *)&saddr, sizeof(saddr)) < 0) {
         printf("BIND ERROR\n");
-        return 0;
+        close(fd);
+        return 1;
     }
 
-    listen(fd, 2);
-    int len = sizeof(caddr);
-    newfd = accept(fd, (struct sockaddr *) &caddr, &len);
-    if(recv(newfd, fname, sizeof(fname), 0) <= 0) {
+    if (listen(fd, 2) < 0) {
+        printf("LISTEN ERROR\n");
+        close(fd);
+        return 1;
+    }
+
+    newfd = accept(fd, (struct sockaddr *)&caddr, (socklen_t *)&addrlen);
+    if (newfd < 0) {
+        printf("ACCEPT ERROR\n");
+        close(fd);
+        return 1;
+    }
+
+    if (recv(newfd, fname, sizeof(fname), 0) <= 0) {
         printf("RECEIVED NO FILENAME\n");
-        return 0;
+        close(newfd);
+        close(fd);
+        return 1;
     }
+
     FILE *f = fopen(fname, "rb");
-    if(f == NULL) {
+    if (f == NULL) {
         printf("FILE NOT FOUND\n");
-        return 0;
+        close(newfd);
+        close(fd);
+        return 1;
     }
-    while(fgets(buff, sizeof(buff), f) != NULL) {
-        if(send(newfd, buff, sizeof(buff, 0), 0) == -1) {
+
+    while (fgets(buff, sizeof(buff), f) != NULL) {
+        size_t bytes_read = strlen(buff);
+        if (send(newfd, buff, bytes_read, 0) == -1) {
             printf("SEND FAILED\n");
-            return 0;
+            fclose(f);
+            close(newfd);
+            close(fd);
+            return 1;
         }
-        memset(buff, 0, sizeof(buff));
     }
+
     printf("FILE TRANSFERRED\n");
-    close(fd);
+    fclose(f);
     close(newfd);
+    close(fd);
+    return 0;
 }
